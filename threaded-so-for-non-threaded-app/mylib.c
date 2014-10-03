ba2405de2de2f5720f5f8a6e8f9656a15819cc18
Oleg G <verdrehung@gmail.com>
2014-10-03 22:18:06 +0700

    and with getaddrinfo?

--- threaded-so-for-non-threaded-app/mylib.c
+++ threaded-so-for-non-threaded-app/mylib.c
@@ -1,9 +1,28 @@
 #include <pthread.h>
 #include <assert.h>
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
 
 void *_thread(void *arg) {
+	int i;
+	//char *buf;
+	struct addrinfo *res;
+	
+	for (i=0; i<10000; i++) {
+		if (getaddrinfo("localhost", NULL, NULL, &res) == 0) {
+			if (res) freeaddrinfo(res);
+		}
+		//buf = malloc(1024*sizeof(char));
+		//strcpy(buf, "abcdefghijklmnopqrstuvwuxyz");
+		//free(buf);
+	}
+	
 	printf("Just another thread message!\n");
+	
 	return NULL;
 }
 
